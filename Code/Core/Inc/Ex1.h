24adb61ab86cacfd952812704cc7ec046bc4ec92
LAPTOP-D1JJHVR7\Mr Duc Anh <anh.ngoducanh@hcmut.edu.vn>
2024-09-27 20:54:41 +0700

    Ex1

---
+++ Code/Core/Inc/Ex1.h
@@ -0,0 +1,13 @@
+#include "main.h"
+#ifndef INC_EX1_H_
+#define INC_EX1_H_
+
+
+
+
+void red(void);
+void yellow(void);
+
+
+
+#endif
---
+++ Code/Core/Src/Ex1.c
@@ -0,0 +1,9 @@
+#include "Ex1.h"
+void red(void){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+}
+void yellow(void){
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
+}
